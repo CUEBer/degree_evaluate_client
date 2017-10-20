f7f683d5b04895a0ded0f5e61e8936d063dac6c8
jiangxingqi <jxq0816@126.com>
2017-10-20 21:49:16 +0800

    jiangxingqi: team

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Router, ActivatedRoute } from "@angular/router";
+import {AuthService} from "../auth.service";
+
+class Team {
+  id: Number;
+  name: String;
+}
 
 @Component({
   selector: 'app-team',
@@ -6,10 +13,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
-
-  constructor() { }
+  @Input() userId: String;
+  team: Team;
+  constructor(public authService: AuthService, public router: Router) {
+  }
 
   ngOnInit() {
+    if (this.userId == null){
+      this.userId = localStorage.getItem('userId');
+    }
+    this.authService.getResource<Team>('http://localhost:8080/school/query').subscribe(
+      data => {
+        this.team = data;
+      },
+      err => {
+        console.log(err);
+        if (err.status === 401) {
+          // invalide token or not logged in
+          this.authService.logout();
+          this.router.navigateByUrl('/');
+        }
+
+      }
+    );
   }
 
 }
